aab95da3394b8d6878fcf3c2eb0fb0e6796d9d0d
Jordan Dale <Jordan.T.Dale@gmail.com>
2014-07-30 12:53:15 -0600

    class method that counts the amount of instances made

--- lib/rectangle.rb
+++ lib/rectangle.rb
@@ -1,16 +1,13 @@
 class Rectangle
+  attr_reader :length, :width
+
+    @@counter = 0
   def initialize(length, width)
     @length = length
-    @width  = width
+    @width = width
+    @@counter += 1
   end
 
-  def length
-    @length
-  end
-
-  def width
-    @width
-  end
 
   def area_calc
     @length * @width
@@ -18,5 +15,13 @@ class Rectangle
 
   def perimeter_calc
     (2 * @length) + (2 * @width)
+  end
+
+  def self.count
+    @@counter += 1
+  end
+
+  def self.how_many_rectangles
+    @@counter
   end
 end
--- spec/rectangle_spec.rb
+++ spec/rectangle_spec.rb
@@ -2,17 +2,31 @@ require 'spec_helper'
 
 describe "initialize a Rectangle with a length and a width" do
   it "the rectangle object returns the length and the width" do
-    rec = Rectangle.new(6,10)
+    rec = Rectangle.new(6, 10)
 
-    expect(rec.length).to  eq(6)
-    expect(rec.width).to   eq(10)
+    expect(rec.length).to eq(6)
+    expect(rec.width).to eq(10)
   end
 
   it "rectangle can return it's area" do
-    expect(Rectangle.new(6,10).area_calc).to eq(60)
+    expect(Rectangle.new(6, 10).area_calc).to eq(60)
   end
 
   it "rectangle can return it's perimeter" do
-    expect(Rectangle.new(6,10).perimeter_calc).to eq(32)
-end
+    expect(Rectangle.new(6, 10).perimeter_calc).to eq(32)
+  end
+
+  describe "class can track how many objects it has made"
+    before do
+      rec_1 = Rectangle.new(6, 3)
+      rec_2 = Rectangle.new(4, 2)
+    end
+
+    after do
+      @@counter = 0
+    end
+    it "rectangle tell you how many Rectangles have been created" do
+
+    expect(Rectangle.how_many_rectangles).to eq(2)
+  end
 end
