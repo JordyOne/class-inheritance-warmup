ed681e78ad68770875f41b2a6b6179f020f575a0
Jordan Dale <Jordan.T.Dale@gmail.com>
2014-07-30 09:55:58 -0600

    Refactored Square class with inheritance

--- lib/square.rb
+++ lib/square.rb
@@ -1,20 +1,6 @@
 
 
-class Square
-  def initialize(all_side_length)
-    @side_length = all_side_length
-  end
+class Square < Rectangle
 
-  def side
-    @side_length
-  end
-
-  def area_calc
-    @side_length**2
-  end
-
-  def perimeter_calc
-    @side_length * 4
-  end
 
 end
--- spec/square_spec.rb
+++ spec/square_spec.rb
@@ -4,17 +4,18 @@ require 'spec_helper'
 
 describe "initialize a Square with a length and a width" do
   it "the rectangle object returns the length and the width" do
-    rec = Square.new(6)
 
-    expect(rec.side).to  eq(6)
+    expect(Square.new(6,6).length).to  eq(6)
+    expect(Square.new(6,6).width).to  eq(6)
+
   end
 
   it "square can return it's area" do
-    expect(Square.new(6).area_calc).to eq(36)
+    expect(Square.new(6,6).area_calc).to eq(36)
   end
 
   it "square can return it's perimeter" do
-    expect(Square.new(6).perimeter_calc).to eq(24)
+    expect(Square.new(6,6).perimeter_calc).to eq(24)
   end
 
 end
