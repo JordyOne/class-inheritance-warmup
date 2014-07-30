38ac0a4abd6046b9f8dcc4a2767bb942698af7bb
Jordan Dale <Jordan.T.Dale@gmail.com>
2014-07-30 09:27:11 -0600

    created rectangle class with attributes

---
+++ lib/rectangle.rb
@@ -0,0 +1,14 @@
+class Rectangle
+  def initialize(length, width)
+    @length = length
+    @width  = width
+  end
+
+  def length
+    @length
+  end
+
+  def width
+    @width
+  end
+end
--- spec/rectangle_spec.rb
+++ spec/rectangle_spec.rb
@@ -1,2 +1,10 @@
 require 'spec_helper'
 
+describe "initialize a Rectangle with a length and a width" do
+  it "the rectangle object returns the length and the width" do
+    rec = Rectangle.new(6,10)
+
+    expect(rec.length).to  eq(6)
+    expect(rec.width).to   eq(10)
+  end
+end
