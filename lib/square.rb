ae7c6c55d2d7d5039575ea9217c51260d4ab4462
Jordan Dale <Jordan.T.Dale@gmail.com>
2014-07-30 09:42:55 -0600

    Square class with test

---
+++ lib/square.rb
@@ -0,0 +1,16 @@
+
+
+class Square
+  def initialize(all_side_length)
+    @side_length = all_side_length
+  end
+
+  def length
+    @side_length
+  end
+
+  def width
+    @side_length
+  end
+
+end
--- spec/square_spec.rb
+++ spec/square_spec.rb
@@ -1,2 +1,11 @@
 require 'spec_helper'
 
+require 'spec_helper'
+
+describe "initialize a Square with a length and a width" do
+  it "the rectangle object returns the length and the width" do
+    rec = Square.new(6)
+
+    expect(rec.length).to  eq(rec.width)
+  end
+end
